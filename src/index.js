ad78b4b8295123f9b82f1053df0b80395581fd37
Pirci <emrepirci@gmail.com>
2022-02-01 16:35:12 +0100

    Index.js resume update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import initTilt from "./js/tilt";
-import initSr from "./js/sr";
-import resume from "./assets/resume.pdf";
+import initTilt from './js/tilt';
+import initSr from './js/sr';
+import resume from './assets/emre-pirci-cv.pdf';
 
-import "./style/main.scss";
+import './style/main.scss';
 
-$('a[href^="#"]').on("click", function (event) {
-  var target = $(this.getAttribute("href"));
-  if (target.length) {
-    event.preventDefault();
-    $("html, body").stop().animate(
-      {
-        scrollTop: target.offset().top,
-      },
-      1000
-    );
-  }
+$('a[href^="#"]').on('click', function (event) {
+	var target = $(this.getAttribute('href'));
+	if (target.length) {
+		event.preventDefault();
+		$('html, body').stop().animate(
+			{
+				scrollTop: target.offset().top,
+			},
+			1000
+		);
+	}
 });
 
 function addResume(pdf) {
-  if (!pdf) return;
+	if (!pdf) return;
 
-  const resumeButton = document.querySelector(".cta-btn--resume");
-  resumeButton.setAttribute("href", pdf);
+	const resumeButton = document.querySelector('.cta-btn--resume');
+	resumeButton.setAttribute('href', pdf);
 }
 
 initSr();
